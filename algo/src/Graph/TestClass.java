fef8bf84b74c7c0276e497528430fac3c19a8c6d
abhilabh <kumarlabh.kumar5@gmail.com>
2019-02-06 18:50:02 +0530

    Toplogical sort of graph

--- algo/src/Graph/TestClass.java
+++ algo/src/Graph/TestClass.java
@@ -3,6 +3,7 @@ package Graph;
 public class TestClass {
     public static void main(String args[]){
         GraphImpl graph = new GraphImpl(6);
+        GraphImplArray graphArray = new GraphImplArray(5);
         /*graph.addNode(0,1);
         graph.addNode(0,4);
         graph.addNode(1,2);
@@ -44,6 +45,10 @@ public class TestClass {
         //graphArray.printGraph();
         graphArray.bfsPrint(2);
         */
+        /*
+            Topological sort
+         */
+        /*
         graph.addNodeAdj(2, 3);
         graph.addNodeAdj(3, 1);
         graph.addNodeAdj(4, 0);
@@ -53,6 +58,63 @@ public class TestClass {
 
         graph.printGraph();
         graph.topologicalSort();
+        */
+        /*
+        min spaning tree using prism
+         */
+        /*
+        Integer graphPrism[][] = new Integer[][] {{0, 2, 0, 6, 0},
+                {2, 0, 3, 8, 5},
+                {0, 3, 0, 0, 7},
+                {6, 8, 0, 0, 9},
+                {0, 5, 7, 9, 0}};
 
+        // Print the solution
+        graphArray.prismMst(graphPrism,5);
+        */
+        /*
+        dijkstra algo
+         */
+        /*
+        Integer graphDjksatra[][] = new Integer[][] {{0, 4, 0, 0, 0, 0, 0, 8, 0},
+                {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                {0, 0, 2, 0, 0, 0, 6, 7, 0}
+        };
+
+        graphArray.djkstraMst(graphDjksatra, 9);
+
+        */
+
+        /*
+        longest path to be continued
+         */
+        /*
+        Integer graphLongDist[][] = new Integer[][]{{0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 3, 0, 0, 0, 0},
+                {0, 3, 0, 4, 0, 0, 2},
+                {0, 0, 4, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 6},
+                {0, 0, 0, 0, 0, 0, 5},
+                {0, 0, 0, 0, 6, 5, 0}
+
+        };
+        graphArray.longestDist(graphLongDist, 7);
+
+        */
+        graphArray.addNode(0, 1);
+        graphArray.addNode(0, 2);
+        graphArray.addNode(0, 4);
+        graphArray.addNode(1, 3);
+        graphArray.addNode(1, 4);
+        graphArray.addNode(3, 2);
+        graphArray.addNode(2, 4);
+
+        System.out.println("no of count from "+0+" to "+ 4+" is "+graphArray.NoOfPath(0,4));
     }
 }
